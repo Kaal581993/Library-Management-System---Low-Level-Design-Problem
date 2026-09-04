ab91384d022e15ed502750df35e9d64dffdfeef6
Viral Prajapati <viral.prajapati.nmims@gmail.com>
2026-09-04 15:26:29 +0530

    Quantity Validation

--- src/validation/book_validation/impl/QuantityValidationHandler.java
+++ src/validation/book_validation/impl/QuantityValidationHandler.java
@@ -3,21 +3,21 @@ package validation.book_validation.impl;
 import entity.BookStatus;
 import validation.book_validation.BookSearchRequest;
 import validation.book_validation.BookValidationHandler;
+import validation.book_validation.ValidationException;
 
 public class QuantityValidationHandler implements BookValidationHandler {
     private BookValidationHandler nextHandler;
     public void setNextHandler(BookValidationHandler nextHandler) {
         this.nextHandler = nextHandler;
     }
-    public boolean handle(BookSearchRequest bookSearchRequest) {
+    public boolean handle(BookSearchRequest bookSearchRequest) throws ValidationException {
         if (
                 bookSearchRequest.getQuantity() < 0
                 || bookSearchRequest.getBookStatus() == BookStatus.LOST
                 ||bookSearchRequest.getBookStatus() == BookStatus.DAMAGED
                 ||bookSearchRequest.getBookStatus() == BookStatus.UNAVAILABLE
         ) {
-            System.out.println("Error: Book quantity cannot be negative or the book is not available");
-            return false;
+            throw new ValidationException("Error: Book quantity cannot be negative or the book is not available");
         }
         System.out.println("Book available: " + bookSearchRequest.getIsbn() +","+bookSearchRequest.getTitle()+ " by " + bookSearchRequest.getAuthor());
         return nextHandler != null && nextHandler.handle(bookSearchRequest);
